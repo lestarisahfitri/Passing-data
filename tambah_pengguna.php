64c705d3203d84d9cd8739885dd6754b4e6fa8ab
lestarisahfitri <102138623+lestarisahfitri@users.noreply.github.com>
2023-01-19 12:43:54 +0700

    Add files via upload

---
+++ tambah_pengguna.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Biodata</title>
+</head>
+<body>
+	<b>FORM TAMBAH DATA</b>
+	<form action="proses_tambah_data.php" method="POST">
+	<table border="1">
+		<tr>
+			<td>
+				NAMA
+			</td>
+			<td>
+				<INPUT name="namaku" TYPE="TEXT" PLACEHOLDER="nama anda">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				PASSWORD
+			</td>
+			<td>
+				<INPUT name="katasandiku" TYPE="PASSWORD" PLACEHOLDER="password anda">
+			</td>
+		</tr>
+		<tr>
+			
+			<td>
+				<INPUT  TYPE="submit" value="Kirim Data">
+			</td>
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
